5f2df5ec9365264e3cc7ef5c7d63306bbe4f2f13
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-04-14 15:16:39 -0500

    input from local storage from user

--- Develop/script.js
+++ Develop/script.js
@@ -13,6 +13,18 @@ $(document).ready(function() {
     localStorage.setItem(hour, description);
   });
 
+    // the saved input from localStorage
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+  });
+
   $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
